7621f535816da08ae6c800584d89fe7d81951b97
Chris Hilton <hilton.chris210@gmail.com>
2022-12-04 17:11:06 -0600

    step 11 done something wied is happening to my .js

--- code.js
+++ code.js
@@ -1,11 +1,23 @@
-// helloWorld function 
-const helloWorld = function helloWorld() {
+const helloWorld = function() {
     return "Hello, World!";
 }
 
 function sayHello(input){
-    if (input === undefined){
-        return 'Hello, World!'
-    } else{
-    return "Hello, " + input + "!"
-}}
+    if(input === undefined || input === true || input === false){
+        return"Hello, World!";
+    } else {
+        return "Hello, " + input + "!";
+    }}
+
+
+function isFive(input) {
+    if (input == 5) {
+        return (true);
+    } else {
+        return (true)
+    }}
+
+function isEven(input) {
+    return (parseFloat(input) % 2 === 0);
+    }
+
--- tests.js
+++ tests.js
@@ -1,37 +1,91 @@
-// Unit tests for the helloWorld function
 describe('helloWorld', function() {
     it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+        expect(typeof helloWorld).toBe('function');
     });
     it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+        expect(typeof helloWorld()).toBe("string");
     });
     it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+        expect(helloWorld()).toBe("Hello, World!");
     });
     it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+        expect(helloWorld()).not.toBe(undefined);
     });
 });
 
-describe( 'sayHello', function () {
-    it('should be defined function', function() {
+describe('sayHello', function (){
+    it('should be a defined function', function (){
         expect(typeof sayHello).toBe('function');
     });
-    it('Should return a string when called', function() {
+    it('should return a string when called', function(){
         expect(typeof sayHello()).toBe('string');
     });
-    it('Should return a string "Hello, Jane"', function() {
-        expect(sayHello("Jane")).toBe('Hello, Jane!');
+    it('should return the string "Hello, Jane!" when executed', function () {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
-    it('sayHello("Alex") should return "Hello, Alex"', function() {
-        expect(sayHello("Alex")).toBe('Hello, Alex!');
+    it('should return the string "Hello, Alex!" when executed', function (){
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    })
+    it('should return the string "Hello, Pat!" when executed', function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
-    it('sayHello("Pat") should return "Hello, Pat"', function() {
-        expect(sayHello("Pat")).toBe('Hello, Pat!');
+    it('should return the string "Hello, World!" when called', function () {
+        expect(sayHello()).toBe("Hello, World!");
     });
-    it('sayHello() should return "Hello, World!"', function() {
-        expect(sayHello()).toBe('Hello, World!');
+    it('should return the string "Hello, World!" when true', function () {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when false', function () {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
+})
+
+describe("isFive", function (){
+    it('should be a defined function', function () {
+        expect(typeof isFive).toBe("function");
+    });
+    it('should breturn a boolean no matter what', function () {
+        expect(typeof isFive).toBe(boolean)
+    });
+    it('should return true if passed the number 5', function () {
+        expect(isFive(5)).toBe(true)
+    });
+    it('should return true if passed the string 5', function () {
+        expect(isFive('5')).toBe(true)
+    });
+    it('should return a boolean', function () {
+        expect(isFive()).toBe(true)
+    });
+})
+describe('isEven', function (){
+    it('should breturn a boolean no matter what', function () {
+        expect(typeof isEven).toBe(boolean)
+    });
+    it('should be a defined function', function () {
+        expect(typeof isEven).toBe("function")
+    });
+    it('should return true when passed 2', function () {
+        expect(isEven(2)).toBe(true)
+    });
+    it('should return true when passed -4', function () {
+        expect(isEven(-4)).toBe(true)
+    });
+    it('should return false when passed 3', function () {
+        expect(isEven(3)).toBe(false)
+    });
+    it('should return false when passed "bannana"', function () {
+        expect(isEven("bannana")).toBe(false)
+    });
+    it('should return true when passed "8"', function () {
+        expect(isEven("8")).toBe(true)
+    });
+    it('should return true when passed infinity', function () {
+        expect(isEven(Infinity)).toBe(false)
+    });
+    it('should return false when passed a boolean input', function () {
+        expect(isEven(true || false)).toBe(false)
+    });
+    it('should return false when called without an argument', function () {
+        expect(isEven()).toBe(false)
     });
 });
-// morning push
